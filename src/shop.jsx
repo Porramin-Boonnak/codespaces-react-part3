2ac8da3977ca401630829d2ca74b71c92cb85e8d
Porramin-Boonnak <121176464+Porramin-Boonnak@users.noreply.github.com>
2024-10-17 08:46:58 +0000

    implement backend

--- src/shop.jsx
+++ src/shop.jsx
@@ -1,17 +1,24 @@
 import './shop.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useRef } from 'react';
 import axios from 'axios';
 const Item=(props)=>{
-    return (<div key={props.id} onClick={()=>props.callback(props)}>
+    return (<div key={props.id} >
         <img src={props.img} width={200} height={200}/><br/>
         id: {props.id}<br/>
         Name: {props.name}<br/>
         Price: {props.price}<br/>
+        <input type='submit' value='update' onClick={()=>props.update(props)}/><br/>
+        <input type='submit' value='addcart' onClick={()=>props.callback(props)}/><br/>
+        <input type='submit' value='Delete' onClick={()=>props.del_callback(props.id)}/>
     </div>)
 }
 export default function Shop(){
+        let id; 
+        const name_ref=useRef(null); 
+        const price_ref=useRef(null); 
+        const img_ref=useRef(null); 
         const [products,setProducts] = useState([])
-        const url = "https://redesigned-goggles-44vrj4v4q9rfqp4j-5000.app.github.dev";
+        const url = "https://miniature-waffle-pvw6jvw4xjw3r4w4-5001.app.github.dev";
         useEffect(()=>{
             axios.get(url+'/api/products').then(response=>{
                 setProducts(response.data);
@@ -22,7 +29,7 @@ export default function Shop(){
 
             }
         },[])
-        const productlist=products.map(item=><Item {...item} callback={addCart}/>)
+        const productlist=products.map(item=><Item {...item} callback={addCart} del_callback={del_product} update={updateProductfrom}/>)
         const [cart,setcart]=useState([]);
         const totalPrice = cart.reduce((total, item) => total + item.price, 0);
         function addCart(item){
@@ -33,7 +40,50 @@ export default function Shop(){
             setcart(cart.filter((i,_index)=>index!=_index));
         }}>
         remove</button></li>)
+        function addproduct(){
+            const data = {
+                name : name_ref.current.value,
+                price:price_ref.current.value,
+                img: img_ref.current.value
+            }
+            axios.post(url+'/api/products',data).then(response=>{
+                setProducts(response.data);
+            }).catch(error=>{
+                console.log("error");
+            });
+        }
+        function del_product(id){
+            axios.delete(url+'/api/products/'+id).then(response=>{
+                setProducts(response.data)
+            }).catch(error=>{
+                console.log("error");
+            });
+        }
+        function updateProductfrom(item){
+            id=item.id;
+            name_ref.current.value=item.name;
+            price_ref.current.value=item.price;
+            img_ref.current.value=item.img;
+        }
+
+        function updateProduct(){
+            const data={
+                name:name_ref.current.value,
+                price:price_ref.current.value,
+                img:img_ref.current.value
+            }
+            axios.put(url+'/api/products/'+id,data).then(response=>{
+                setProducts(response.data)
+            }).catch(error=>{
+                console.log("error");
+            });
+        }
         return (<>
+            name :<input type='text' ref={name_ref}/>
+            price :<input type='text' ref={price_ref}/>
+            img :<input type='text' ref={img_ref}/>
+            <input type='submit' value='add' onClick={addproduct}/>
+            <input type='submit' value='update' onClick={updateProduct}/>
             <div className='grid-container'>{productlist}</div>
             <h1>Cart</h1>
             <h1>All price {totalPrice}</h1>
